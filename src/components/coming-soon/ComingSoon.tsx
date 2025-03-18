fbba7652e3f580dd0dcabbd335c07ee3aa8ebe03
Denny McFadden <dennymcfadden@gmail.com>
2025-03-17 21:57:11 -0400

    Update L/R Margins

--- src/components/coming-soon/ComingSoon.tsx
+++ src/components/coming-soon/ComingSoon.tsx
@@ -9,7 +9,7 @@ export const ComingSoon: React.FC = () => {
     <section 
       aria-labelledby="coming-soon-title" 
       className="flex items-center gap-[40px_94px] overflow-hidden justify-center flex-wrap
-        px-5 sm:px-8 md:px-12 lg:px-24 xl:px-32 2xl:px-48
+        px-5 sm:px-8 md:px-12 lg:px-32 xl:px-48 2xl:px-64
         py-[68px]"
     >
       <div className="self-stretch min-w-60 flex-1 shrink basis-[0%] my-auto max-md:max-w-full">
